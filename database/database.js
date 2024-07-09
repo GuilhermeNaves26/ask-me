18858abed52810ee85a097e89a000f523bad971e
GuilhermeNaves26 <mafraguilherme309@gmail.com>
2024-07-09 10:44:45 -0300

    configuração do .env e banco de dados

--- database/database.js
+++ database/database.js
@@ -1,15 +1,14 @@
+require('dotenv').config()
 const { Sequelize } = require('@sequelize/core')
-const { MariaDbDialect } = require('@sequelize/mariadb')
+const { PostgresDialect } = require('@sequelize/mariadb')
 
 const connection = new Sequelize({
-    dialect: MariaDbDialect,
-    database: 'askme',
-    user: 'administrador',
-    password: '123456',
-    host: '172.16.96.108',
-    port: 3306,
-    showWarnings: true,
-    connectTimeout: 1000,
+    dialect: PostgresDialect,
+    database: process.env.DB_NAME,
+    user: process.env.USER,
+    password:process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.DB_PORT,
 })
 
 
